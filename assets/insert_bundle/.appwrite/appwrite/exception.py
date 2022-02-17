cffbc329ca99341ee864b8edc231c95e4fce9496
tannermeade <tannersmeade@gmail.com>
2022-02-17 10:53:23 -0700

    removed unnecesary cloud function files

--- assets/insert_bundle/.appwrite/appwrite/exception.py
+++
@@ -1,6 +0,0 @@
-class AppwriteException(Exception):
-    def __init__(self, message, code = 0, response = None):
-        self.message = message
-        self.code = code
-        self.response = response
-        super().__init__(self.message)
